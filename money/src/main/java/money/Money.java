862217fdc77069c4dbc5b3947d80a25efd40dbf7
oohira <oohira.dev@gmail.com>
2018-02-26 08:57:22 +0900

    Done: timesの一般化

--- money/src/main/java/money/Money.java
+++ money/src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-abstract class Money {
+class Money {
 
     protected int amount;
     protected String currency;
@@ -10,7 +10,9 @@ abstract class Money {
         this.currency = currency;
     }
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     String currency() {
         return currency;
@@ -20,7 +22,12 @@ abstract class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 
     static Money dollar(int amount) {
